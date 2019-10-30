af6c93cc1bce4327eb20eb07e02780b02ecab260
TranVanDu <du.tv08@gmail.com>
2019-10-30 22:55:33 +0700

    component

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+    render(){
+        return (
+           <div>
+               Hello, It is TodoList
+           </div>
+        );
+    }
+}
+
+export default TodoItem;
